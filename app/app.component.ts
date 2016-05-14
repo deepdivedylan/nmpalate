5004dd29e989e8ab62e5dde4c193f7049c647b20
brbrown59 <tall.white.ninja@gmail.com>
2016-05-13 23:06:34 -0600

    set default template back to splash

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,6 +2,6 @@ import {Component} from "@angular/core";
 
 @Component({
 	selector: "nm-pallete",
-	templateUrl: "app/templates/result.html"
+	templateUrl: "app/templates/splash.html"
 })
 export class NmPallete { }
