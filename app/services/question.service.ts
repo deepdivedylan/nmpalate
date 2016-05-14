76233de607c402cb5ec7f0c5d4af395df4a95f04
Dylan McDonald <dmcdonald21@cnm.edu>
2016-05-14 14:16:04 -0600

    corrected silly typographical error

--- app/services/question.service.ts
+++ app/services/question.service.ts
@@ -6,27 +6,26 @@ import {Answers} from '../classes/answers';
 
 @Injectable()
 export class QuestionService {
-    constructor(private http: Http) {}
-    private questionUrl = "/app/api/question.json";
-    
-    getQuestion() : Observable<Question[]> {
-        return(this.http.get(this.questionUrl)
-            .map(this.extractData)
-            .catch(this.handleError));
-    }
-    
-    private extratData(response: Response) {
-        if(response.status <  200 || response.status >= 300) {
-            throw(new Error("Bad response status: " + response.status))
-        }
-        
-        return(response.json());
-    }
-    
-    private handleError(error: any) {
-        let message = error.message;
-        console.log(message);
-        return(Observable.throw(message));
-    }
+	constructor(private http: Http) {}
+	private questionUrl = "/app/api/question.json";
 
+	getQuestion() : Observable<Question[]> {
+		return(this.http.get(this.questionUrl)
+			.map(this.extractData)
+			.catch(this.handleError));
+	}
+
+	private extractData(response: Response) {
+		if(response.status <  200 || response.status >= 300) {
+			throw(new Error("Bad response status: " + response.status))
+		}
+
+		return(response.json());
+	}
+
+	private handleError(error: any) {
+		let message = error.message;
+		console.log(message);
+		return(Observable.throw(message));
+	}
 }
