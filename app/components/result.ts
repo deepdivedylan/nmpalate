40efd1a43706240c409a21e45d93feea576a2a14
brbrown59 <tall.white.ninja@gmail.com>
2016-05-14 12:17:33 -0600

    twitter button now works and is styled

--- app/components/result.ts
+++ app/components/result.ts
@@ -1,7 +1,9 @@
 import {Component} from "@angular/core";
 
 @Component({
-    templateUrl: "app/templates/result.html"
+    templateUrl: "app/templates/result.html",
+    styleUrls: ['app/css/result.css']
+
 })
 
 export class ResultComponent {}
