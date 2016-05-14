2383dd68793e843046b7625581ff3b3eb059b43e
Francisco Garcia <ciscog11@msn.com>
2016-05-14 16:19:53 -0600

    added beer.service.ts https://app.asana.com/0/133249000295647/133249000295697

---
+++ app/services/beer.service.ts
@@ -0,0 +1,33 @@
+import {Injectable} from "@angular/core";
+import {Http,Response} from "@angular/http";
+import {Observable} from "rxjs/Observable";
+import {Beer} from "../classes/beer";
+import {TasteCoordinates} from "../classes/taste-coordinates";
+
+
+@Injectable()
+export class BeerService{
+	constructor(private http: Http){}
+
+	private beerUrl= "/app/api/beer.json";
+
+	getBeer(): Observable<Beer[]>{
+		return(this.http.get(this.beerUrl)
+			.map(this.extractData)
+			.catch(this.handleError));
+	}
+
+	private extractData(response: Response){
+		if(response.status < 200 || response.status >= 300) {
+			throw(new Error("Bad response tatus: " + response.status))
+		}
+		return(response.json());
+	}
+
+	private handleError(error:any){
+
+		let message = error.message;
+		console.log(message);
+		return(Observable.throw(message));
+	}
+}
