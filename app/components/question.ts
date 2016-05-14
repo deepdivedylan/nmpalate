72a376cf45905ae71b62ef33387dfb8833ce1d80
brbrown59 <tall.white.ninja@gmail.com>
2016-05-14 14:38:56 -0600

    wrote the answers service

--- app/components/question.ts
+++ app/components/question.ts
@@ -1,5 +1,8 @@
-import {Component} from "@angular/core";
-import {QuestionService} from "../services/question.service"
+import {Component, OnInit} from "@angular/core";
+import {QuestionService} from "../services/question.service";
+import {AnswersService} from "../services/answers.service";
+import {Question} from "../classes/question";
+import {Answers} from "../classes/answers";
 
 
 @Component({
@@ -7,5 +10,11 @@ import {QuestionService} from "../services/question.service"
 })
 
 export class QuestionComponent {
-    constructor(private QuestionService: QuestionService) {}
+    constructor(private QuestionService: QuestionService, private AnswersService: AnswersService) {}
+    questionList: Question[];
+    answerList: Answers[];
+    
+    ngOnInit() {
+    }
+    
 }
