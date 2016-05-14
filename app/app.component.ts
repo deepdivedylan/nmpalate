26fa9b7f31096ce24de54c0f24f678415176341b
Dylan McDonald <dmcdonald21@cnm.edu>
2016-05-14 11:13:22 -0600

    Refactored nav to be Angularific - this is insane!

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit} from "@angular/core";
+import {NgClass} from "@angular/common";
 import {Routes, Router, ROUTER_DIRECTIVES} from "@angular/router";
 import {AboutComponent} from "./components/about";
 import {SplashComponent} from "./components/splash";
@@ -9,7 +10,7 @@ import {QuestionService} from "./services/question.service"
 @Component({
 	selector: "nm-pallete",
 	templateUrl: "app/templates/nm-pallete.html",
-	directives: [ROUTER_DIRECTIVES],
+	directives: [NgClass, ROUTER_DIRECTIVES],
 	providers: [QuestionService]
 })
 
@@ -23,4 +24,10 @@ import {QuestionService} from "./services/question.service"
 
 export class NmPallete {
 	constructor(private router: Router) {}
+
+	navCollapse = true;
+
+	toggleCollapse() {
+		this.navCollapse = !this.navCollapse;
+	}
 }
