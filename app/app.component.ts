e70abccbfb3fd57ebfc1be3bedb35bfbafba25a6
brbrown59 <tall.white.ninja@gmail.com>
2016-05-14 01:36:51 -0600

    set up skeleton result component in order to route to page

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,6 +2,7 @@ import {Component, OnInit} from "@angular/core";
 import {Routes, Router, ROUTER_DIRECTIVES} from "@angular/router";
 import {AboutComponent} from "./components/about";
 import {SplashComponent} from "./components/splash";
+import {ResultComponent} from "./components/result";
 
 @Component({
 	selector: "nm-pallete",
@@ -10,6 +11,7 @@ import {SplashComponent} from "./components/splash";
 })
 
 @Routes([
+    {path: "/result", component: ResultComponent},
 	{path: "/about", component: AboutComponent},
 	{path: "/", component: SplashComponent}
 ])
---
+++ app/components/result.ts
@@ -0,0 +1,7 @@
+import {Component} from "@angular/core";
+
+@Component({
+    templateUrl: "app/templates/result.html"
+})
+
+export class ResultComponent {}
