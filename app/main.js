c0244c6235b1426980d0d0e21ce8d53abf35fd09
Dylan McDonald <dmcdonald21@cnm.edu>
2016-05-13 20:19:59 -0600

    Added splash place holder

--- app/main.js
+++ app/main.js
@@ -1,5 +1,5 @@
 (function(app) {
-  document.addEventListener('DOMContentLoaded', function() {
-    ng.platformBrowserDynamic.bootstrap(app.AppComponent);
-  });
+	document.addEventListener('DOMContentLoaded', function() {
+		ng.platformBrowserDynamic.bootstrap(app.AppComponent);
+	});
 })(window.app || (window.app = {}));
