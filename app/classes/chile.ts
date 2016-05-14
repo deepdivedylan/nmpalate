b532ad22d64372ee69d3e2bf200ba7d61f0841be
Eliot Ostling <it.treugott@gmail.com>
2016-05-14 13:03:53 -0600

    Adding chile

---
+++ app/classes/chile.ts
@@ -0,0 +1,11 @@
+import {TasteCoordinates} from "./taste-coordinates";
+
+export class Chile{
+	constructor(
+		public name: string,
+		public description: string,
+		public imgUrl: string,
+		public coordinates: TasteCoordinates
+
+	){}
+}
