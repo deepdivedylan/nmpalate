d2c36308152ec94c8d3279278516b7b2cad16795
Dylan McDonald <dmcdonald21@cnm.edu>
2016-05-15 07:09:26 -0600

    added id to beer class

--- app/classes/beer.ts
+++ app/classes/beer.ts
@@ -2,10 +2,10 @@ import {TasteCoordinates} from "./taste-coordinates";
 
 export class Beer{
 	constructor(
+		public id: number,
 		public name: string,
 		public description: string,
 		public imgUrl: string,
 		public coordinates: TasteCoordinates
-
 	){}
 }
