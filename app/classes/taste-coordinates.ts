b6e0e2e407c2a0578bd1b75f7507a091bb2fe275
Dylan McDonald <dmcdonald21@cnm.edu>
2016-05-15 11:44:29 -0600

    added Euclidean distance to TasteCoordinates

--- app/classes/taste-coordinates.ts
+++ app/classes/taste-coordinates.ts
@@ -4,4 +4,10 @@ export class TasteCoordinates {
 		public spice: number,
 		public sweet: number
 	) {}
+
+	euclideanDistance(x: TasteCoordinates) {
+		let squares = Math.pow(x.savory - this.savory, 2.0) + Math.pow(x.spice - this.spice, 2.0) + Math.pow(x.sweet - this.sweet, 2.0);
+		let distance = Math.sqrt(squares);
+		return(distance);
+	}
 }
